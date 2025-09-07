c3f20c61178b11cc131c145ed159714a701dd07f
Dan Ivan E. Labin <danivanlabin@gmail.com>
2025-09-07 23:10:06 +0800

    final

---
+++ App.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.name}>Dan Ivan E. Labin</Text>
+      <Text style={styles.course}>BSIT 3rd Year - IT3R12_Track2</Text>
+      <Text style={styles.message}>
+        This is my first React Native app using View and Text!
+      </Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#87CEEB',
+    padding: 20,
+  },
+  name: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    marginBottom: 15,
+  },
+  course: {
+    fontSize: 20,
+    marginBottom: 10,
+  },
+  message: {
+    fontSize: 20,
+    textAlign: 'center',
+  },
+});
